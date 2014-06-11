77c69bcf1f78c17f421cb2b7feeb74704de93a6e
Valery Kholodkov <valery@grid.net.ru>
2014-06-11 18:54:32 +0200

    Populating lists and dropdowns in the Ship Design Dialog

--- QtClient/ship_design_dialog.cpp
+++ QtClient/ship_design_dialog.cpp
@@ -3,57 +3,189 @@
  */
 #include <cmath>
 
+#include <QSignalMapper>
+
 #include "ship_design_dialog.h"
 
 namespace FreeStars {
 
+struct CompCategory {
+    const char *title;
+    long mask;
+};
+
+static CompCategory starbaseCategories[] = {
+    { "All", CT_ARMOR | CT_SHIELD | CT_WEAPON | CT_ELEC | CT_MECH | CT_ORBITAL },
+    { "Armor", CT_ARMOR },
+    { "Shields", CT_SHIELD },
+    { "Weapons", CT_WEAPON },
+    { "Electrical", CT_ELEC },
+    { "Mechanical", CT_MECH },
+    { "Orbital", CT_ORBITAL },
+    { NULL, 0 }
+};
+
+static CompCategory shipCategories[] = {
+    { "All", CT_ARMOR | CT_SHIELD | CT_WEAPON | CT_BOMB | CT_ELEC | CT_ENGINE | CT_MINELAY 
+        | CT_MINER | CT_SCANNER | CT_MECH },
+    { "Armor", CT_ARMOR },
+    { "Shields", CT_SHIELD },
+    { "Weapons", CT_WEAPON },
+    { "Bombs", CT_BOMB },
+    { "Electrical", CT_ELEC },
+    { "Engines", CT_ENGINE },
+    { "Mine layers", CT_MINELAY },
+    { "Mining Robots", CT_MINER },
+    { "Scanner", CT_SCANNER },
+    { "Mechanical", CT_MECH },
+    { NULL, 0 }
+};
+
 ShipDesignDialog::ShipDesignDialog(Player *_player, QWidget *parent)
     : QDialog(parent)
     , player(_player)
+    , currentDesignMode(SDDDM_SHIPS)
+    , currentViewMode(SDDVM_EXISTING)
 {
     setupUi(this);
 
-    connect(shipDesignsButton, SIGNAL(clicked(bool)), this, SLOT(showShipDesigns()));
-    connect(starbaseDesignsButton, SIGNAL(clicked(bool)), this, SLOT(showStarbaseDesigns()));
+    QSignalMapper *designModeMapper = new QSignalMapper(this);
 
-    connect(existingDesignsButton, SIGNAL(clicked(bool)), this, SLOT(showExistingDesigns()));
-    connect(availableHullTypesButton, SIGNAL(clicked(bool)), this, SLOT(showAvailableHullTypes()));
-    connect(enemyHullsButton, SIGNAL(clicked(bool)), this, SLOT(showEnemyHulls()));
-    connect(componentsButton, SIGNAL(clicked(bool)), this, SLOT(showComponents()));
+    designModeMapper->setMapping(shipDesignsButton, SDDDM_SHIPS);
+    designModeMapper->setMapping(starbaseDesignsButton, SDDDM_STARBASES);
+
+    connect(shipDesignsButton, SIGNAL(clicked(bool)), designModeMapper, SLOT(map()));
+    connect(starbaseDesignsButton, SIGNAL(clicked(bool)), designModeMapper, SLOT(map()));
+
+    QSignalMapper *viewModeMapper = new QSignalMapper(this);
+
+    viewModeMapper->setMapping(existingDesignsButton, SDDVM_EXISTING);
+    viewModeMapper->setMapping(availableHullTypesButton, SDDVM_AVAILABLE);
+    viewModeMapper->setMapping(enemyHullsButton, SDDVM_ENEMY);
+    viewModeMapper->setMapping(componentsButton, SDDVM_COMPONENTS);
+
+    connect(existingDesignsButton, SIGNAL(clicked(bool)), viewModeMapper, SLOT(map()));
+    connect(availableHullTypesButton, SIGNAL(clicked(bool)), viewModeMapper, SLOT(map()));
+    connect(enemyHullsButton, SIGNAL(clicked(bool)), viewModeMapper, SLOT(map()));
+    connect(componentsButton, SIGNAL(clicked(bool)), viewModeMapper, SLOT(map()));
+
+    connect(designModeMapper, SIGNAL(mapped(int)), this, SLOT(setDesignMode(int)));
+    connect(viewModeMapper, SIGNAL(mapped(int)), this, SLOT(setViewMode(int)));
 
     connect(copyDesignButton, SIGNAL(clicked(bool)), this, SLOT(copyDesign()));
     connect(editDesignButton, SIGNAL(clicked(bool)), this, SLOT(editDesign()));
     connect(deleteDesignButton, SIGNAL(clicked(bool)), this, SLOT(deleteDesign()));
 
     connect(doneButton, SIGNAL(clicked(bool)), this, SLOT(accept()));
+
+    populateExistingDesigns(SDDDM_SHIPS);
 }
 
-void ShipDesignDialog::showShipDesigns()
+void ShipDesignDialog::setDesignMode(int mode)
 {
+    switchMode(currentDesignMode, mode, currentViewMode, currentViewMode);
+    currentDesignMode = mode;
 }
 
-void ShipDesignDialog::showStarbaseDesigns()
+void ShipDesignDialog::setViewMode(int mode)
 {
+    switchMode(currentDesignMode, currentDesignMode, currentViewMode, mode);
+    currentViewMode = mode;
 }
 
-void ShipDesignDialog::showExistingDesigns()
+void ShipDesignDialog::switchMode(int oldDesignMode, int newDesignMode, int oldViewMode, int newViewMode)
 {
-    stackedWidget2->setCurrentIndex(0);
+    if (oldViewMode == SDDVM_COMPONENTS) {
+        chooseComponentBox1->clear();
+        chooseComponentBox1->disconnect();
+
+        if (newViewMode != SDDVM_COMPONENTS) {
+            copyDesignButton->setEnabled(true);
+            editDesignButton->setEnabled(true);
+            deleteDesignButton->setEnabled(true);
+        }
+    }
+    else {
+        chooseDesignBox->clear();
+
+        if (oldViewMode == SDDVM_AVAILABLE || oldViewMode == SDDVM_ENEMY) {
+            deleteDesignButton->setEnabled(true);
+        }
+    }
+
+    stackedWidget2->setCurrentIndex(newViewMode == SDDVM_COMPONENTS ? 1 : 0);
+
+    if (newViewMode == SDDVM_COMPONENTS) {
+        struct CompCategory *categories = newDesignMode == SDDDM_SHIPS ? shipCategories : starbaseCategories;
+
+        while (categories->title != NULL) {
+            chooseComponentBox1->addItem(tr(categories->title), QVariant((qlonglong)categories->mask));
+            categories++;
+        }
+
+        connect(chooseComponentBox1, SIGNAL(activated(int)), this, SLOT(setComponentCategory(int)));
+        setComponentCategory(0);
+
+        if (oldViewMode != SDDVM_COMPONENTS) {
+            copyDesignButton->setEnabled(false);
+            editDesignButton->setEnabled(false);
+            deleteDesignButton->setEnabled(false);
+        }
+    }
+    else {
+        if (newViewMode == SDDVM_EXISTING) {
+            populateExistingDesigns(newDesignMode);
+        }
+        else if (newViewMode == SDDVM_AVAILABLE) {
+            populateAvailableHullTypes(newDesignMode);
+        }
+
+        if (newViewMode == SDDVM_AVAILABLE || newViewMode == SDDVM_ENEMY) {
+            deleteDesignButton->setEnabled(false);
+        }
+    }
 }
 
-void ShipDesignDialog::showAvailableHullTypes()
+void ShipDesignDialog::setComponentCategory(int index)
 {
-    stackedWidget2->setCurrentIndex(0);
+    QVariant userData = chooseComponentBox1->itemData(index);
+
+    long mask = userData.toULongLong();
+
+    componentListWidget1->clear();
+
+    const std::deque<Component*> &components = TheGame->GetComponents();
+
+    for (std::deque<Component*>::const_iterator i = components.begin() ; i != components.end() ; i++) {
+        if ((*i)->IsBuildable(player) && ((*i)->GetType() & mask)) {
+            componentListWidget1->addItem(new QListWidgetItem(QString((*i)->GetName().c_str())));
+        }
+    }
 }
 
-void ShipDesignDialog::showEnemyHulls()
+void ShipDesignDialog::populateExistingDesigns(int designMode)
 {
-    stackedWidget2->setCurrentIndex(0);
+    int max = designMode == SDDDM_SHIPS ? Rules::GetConstant("MaxShipDesigns") 
+        : Rules::GetConstant("MaxBaseDesigns");
+
+    for (long i = 0 ; i != max ; i++) {
+        const Ship *ship = designMode == SDDDM_SHIPS ? player->GetShipDesign(i) : player->GetBaseDesign(i);
+
+        if (ship != NULL) {
+            chooseDesignBox->addItem(QString(ship->GetName().c_str()));
+        }
+    }
 }
 
-void ShipDesignDialog::showComponents()
+void ShipDesignDialog::populateAvailableHullTypes(int designMode)
 {
-    stackedWidget2->setCurrentIndex(1);
+    const std::deque<Component*> &components = TheGame->GetComponents();
+
+    for (std::deque<Component*>::const_iterator i = components.begin(); i != components.end(); i++) {
+        if ((*i)->IsBuildable(player) && ((*i)->IsType(designMode == SDDDM_SHIPS ? CT_HULL : CT_BASE))) {
+            chooseDesignBox->addItem(QString((*i)->GetName().c_str()));
+        }
+    }
 }
 
 void ShipDesignDialog::copyDesign()
--- QtClient/ship_design_dialog.h
+++ QtClient/ship_design_dialog.h
@@ -14,27 +14,38 @@
 
 namespace FreeStars {
 
+struct CompCategory;
+
 class ShipDesignDialog : public QDialog, private Ui_ShipDesignDialog {
     Q_OBJECT
+
+    typedef enum {
+        SDDDM_NONE, SDDDM_SHIPS, SDDDM_STARBASES
+    } design_mode_t;
+
+    typedef enum {
+        SDDVM_NONE, SDDVM_EXISTING, SDDVM_AVAILABLE, SDDVM_ENEMY, SDDVM_COMPONENTS
+    } view_mode_t;
 
 public:
     ShipDesignDialog(Player*, QWidget *parent = 0);
 
-public slots:
-    void showShipDesigns();
-    void showStarbaseDesigns();
-
-    void showExistingDesigns();
-    void showAvailableHullTypes();
-    void showEnemyHulls();
-    void showComponents();
-
+private slots:
+    void setDesignMode(int);
+    void setViewMode(int);
+    void setComponentCategory(int);
     void copyDesign();
     void editDesign();
     void deleteDesign();
 
 private:
+    void switchMode(int, int, int, int);
+    void populateExistingDesigns(int);
+    void populateAvailableHullTypes(int);
+
+private:
     Player *player;
+    int currentDesignMode, currentViewMode;
 };
 
 };
